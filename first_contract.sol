4e5b8d92e76696f2608f5e8d99c53c3bb3c78529
Randy <randy.joseph.avalos@gmail.com>
2018-05-27 17:36:41 -0700

    Add Description of Contract
    Add description of contract variables and functions

--- first_contract.sol
+++ first_contract.sol
@@ -1,18 +1,28 @@
-pragma solidity ^0.4.17;
-// First Contract
-contract Inbox {
-    string public message; // defines new public variable "message"
+pragma solidity ^0.4.17; // Specifies version of solidity
+
+// First Contract, contract is similar to a new class with number of methods and variables
+contract Inbox { 
+    string public message; // defines new public string storage variables for contract instance
+                            // this is will be stored on the blockchain for life of blockchain    
     
     function Inbox(string initialMessage) public {
         message = initialMessage;
-    }
+    }   // note the capital I for Inbox being same name as contract name
+        // this is a CONSTRUCTOR function, that is invoked automatically upon contract creation on blockchain
+        
     
+    // Functions below (not named the same as contract) can be called after the contract has been placed on the blockchain
+   
     function setMessage(string newMessage) public {
         message = newMessage;
     }
     
+    // changes message to newMessage
+    
     function getMessage() public view returns (string) {
         return message;
     }
+    
+    // getMesssage returns a view of the variable messae
     
 }

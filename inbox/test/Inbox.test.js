95ccb78fc308d8b4cc9472bc2912967a47de36fb
Randy <randy.joseph.avalos@gmail.com>
2018-06-08 12:31:36 -0700

    Add Await
    Add await to testing framework to account for ansync behavior of ethereum.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,26 +3,25 @@ const ganache = require('ganache-cli'); //local ethereum test network
 const Web3 = require('web3');  // UPPERCASE Web3, is a contructor. Convention is to capitalize constructor functions
 const web3 = new Web3(ganache.provider()); // intance of web3 that will attempt to connect to local ganache network
 
-beforeEach(() => {
+let accounts; // predefine accounts variable
+
+beforeEach(async () => {
 	// Get a list of all accounts
-	//web3.eth.getAccounts()  
-	//	.then(fetchedAccounts => {
-	//		console.log(fetchedAccounts);
-	//	});
-	web3.eth.getAccounts(function(error, result){
+	//accounts = await web3.eth.getAccounts();
+
+	accounts = await web3.eth.getAccounts(function(error, fetchedAccounts){
 		if(!error)
-			console.log(JSON.stringify(result));
+			console.log(JSON.stringify(fetchedAccounts));
 		else
 			console.error(error);
 	});
 
 
 	// Use one of these accounts to deploy the contract
-
 });
 
 describe('Inbox', () => {
 	it('deploys a contract', () => {
-
+		console.log(accounts);
 	});
 });  // just putting in a dummy test to have the task complete with console log

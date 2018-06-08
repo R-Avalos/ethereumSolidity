0e3f66a1e0958c12d86ad2537f3117ab4c7c6f8a
Randy <randy.joseph.avalos@gmail.com>
2018-06-08 12:52:10 -0700

    Fixed Contract Deploy Local
    Fixed contract deploy local for web3 1.0. Course seems to be written for web3 < 1.0

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,8 +2,11 @@ const assert = require('assert'); //assertions about test, x = x
 const ganache = require('ganache-cli'); //local ethereum test network
 const Web3 = require('web3');  // UPPERCASE Web3, is a contructor. Convention is to capitalize constructor functions
 const web3 = new Web3(ganache.provider()); // intance of web3 that will attempt to connect to local ganache network
+const { interface, bytecode } = require('../compile'); //pull the compiled bytecode and ABI
+
 
 let accounts; // predefine accounts variable
+let inbox;
 
 beforeEach(async () => {
 	// Get a list of all accounts
@@ -18,10 +21,16 @@ beforeEach(async () => {
 
 
 	// Use one of these accounts to deploy the contract
+	//inbox = await new web3.eth.contract(JSON.parse(interface)) // constructor function, creating instance of function
+	//	.deploy({ data: bytecode, arguments: ['Hello blocks!'] }) // deploy arguments and initial properties
+	//	.send({ from: accounts[0], gas: '1000000' }) //deploy contract from 1st account
+	inbox = new web3.eth.contract(interface)
+
 });
+
 
 describe('Inbox', () => {
 	it('deploys a contract', () => {
-		console.log(accounts);
+		console.log(inbox);
 	});
 });  // just putting in a dummy test to have the task complete with console log

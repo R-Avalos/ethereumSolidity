afcfed209748df31ccdf6c1c6f73b06a7fb19929
Randy <randy.joseph.avalos@gmail.com>
2018-06-08 09:55:24 -0700

    Add beforeEach Test
    Add beforeEach function and Let for global varialbe. Used for testing multiple assertions.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -11,24 +11,28 @@ const web3 = new Web3(ganache.provider()); // intance of web3 that will attempt 
 class Car {
 	park(){
 		return 'stopped';
-	}
+	};
 
 	drive(){
-		return "vroom";
-	}
-}
+		return 'vroom';
+	};
+};
 
 // Mocha assertions
+let car; // remove const from within function... car has been initalized and undefined, hence let
+
+beforeEach(() => {
+	car = new Car();
+});
 
 describe('Car', () => {
 	it('has a park string function', () => {
-		const car = new Car();
 		assert.equal(car.park(), 'stopped'); // value it is, value it should be
 	});
+
 	it('can drive'), () => {
-		const car = new Car();
 		assert.equal(car.drive(), 'vroom');
-	}
-}) // describe(first variable is the name of the test class, arrow to another function)
+	};
+}); // describe(first variable is the name of the test class, arrow to another function)
 
 // npm run test   // on command line to run tests

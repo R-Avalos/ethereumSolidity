6db9c829e15cffe726ee341c15b1d0f695452e32
Randy <randy.joseph.avalos@gmail.com>
2018-06-08 10:03:46 -0700

    Add Mocha Test Diagram
    Add diagram of mocha testing flow.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,4 +1,4 @@
 const assert = require('assert'); //assertions about test, x = x
 const ganache = require('ganache-cli'); //local ethereum test network
-const Web3 = require('web3');  // UPPERCASE Web3, it is a contructor. Convention is to capitalize constructor functions
+const Web3 = require('web3');  // UPPERCASE Web3, is a contructor. Convention is to capitalize constructor functions
 const web3 = new Web3(ganache.provider()); // intance of web3 that will attempt to connect to local ganache network

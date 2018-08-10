5c2edc8b58e04901863a1646cffe4fe307e2df5b
Randy <randy.joseph.avalos@gmail.com>
2018-08-09 18:47:14 -0700

    Fix web3 version
    Versoin web3 1.0 beta 26 had broken packages, updated with new lines

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,9 +1,11 @@
 const assert = require('assert'); //assertions about test, x = x
 const ganache = require('ganache-cli'); //local ethereum test network
 const Web3 = require('web3');  // UPPERCASE Web3, is a contructor. Convention is to capitalize constructor functions
-const web3 = new Web3(ganache.provider()); // intance of web3 that will attempt to connect to local ganache network
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile'); //pull the compiled bytecode and ABI
-const version = web3.version.api;
 console.log('Web3 version ' + web3.version); //list web3 version
 
 let inbox; // predefine inbox
@@ -13,13 +15,16 @@ beforeEach(async () => {
 	// Get a list of all accounts
 	accounts = await web3.eth.getAccounts();
 	
+	// deploy contract with account 0
 	inbox = await new web3.eth.Contract(JSON.parse(interface))
 		.deploy({data: bytecode, arguments: ['Hi there!']})
 		.send({from: accounts[0], gas: '1000000' });
+	
+	inbox.setProvivder(provider);
 });
 
 describe('Inbox', () => {
 	it('deploys a contract', () => {
-		assert.ok(inbox.options.address); // Deteremine if address has been set for contract
+		assert.ok(inbox.options.address); // Determine if address has been set for contract
 	});
 });  // just putting in a dummy test to have the task complete with console log

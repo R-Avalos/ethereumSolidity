a1175a7177a5a523cb5423211134a31f4330af06
Randy <randy.joseph.avalos@gmail.com>
2018-10-04 20:06:15 -0700

    Fix HD Wallet
    Fix HD wallet

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require('truffle-hdwallet-HDWalletProvider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
